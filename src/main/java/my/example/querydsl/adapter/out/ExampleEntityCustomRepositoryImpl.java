ce709b37a5032c8d2bf7d1e1f842d4430339f661
jo-minjun <alswns9288@gmail.com>
2023-03-24 00:26:22 +0900

    nullSafeBuilder

--- src/main/java/my/example/querydsl/adapter/out/ExampleEntityCustomRepositoryImpl.java
+++ src/main/java/my/example/querydsl/adapter/out/ExampleEntityCustomRepositoryImpl.java
@@ -2,11 +2,16 @@ package my.example.querydsl.adapter.out;
 
 import static my.example.querydsl.domain.QExampleEntity.exampleEntity;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.FactoryExpressionBase;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import my.example.querydsl.application.port.out.ExampleEntityDto;
 import my.example.querydsl.domain.ExampleEntity;
 import my.example.querydsl.domain.ExampleEntity.Type;
@@ -33,14 +38,32 @@ public class ExampleEntityCustomRepositoryImpl implements ExampleEntityCustomRep
     return Mapper.of(exampleEntityDtos);
   }
 
+  private BooleanBuilder commonFieldEq(Integer commonField) {
+    return nullSafeBuilder(() -> exampleEntity.commonField.eq(commonField));
+  }
+
+  private BooleanBuilder typeEq(Type type) {
+    return nullSafeBuilder(() -> exampleEntity.type.eq(type));
+  }
+
+  // or조건을 사용해도 NPE가 발생하지 않도록
+  // e.g. where(typeEq(type).or(commonFiledEq(commonFiled))
+  public static BooleanBuilder nullSafeBuilder(Supplier<Predicate> expression) {
+    try {
+      return new BooleanBuilder(expression.get());
+    } catch (IllegalArgumentException e) {
+      return new BooleanBuilder();
+    }
+  }
+
   private static FactoryExpressionBase<ExampleEntityDto> getSelectFieldsByType(Type type) {
     return switch (type) {
       case A -> Projections.bean(ExampleEntityDto.class, exampleEntity.commonField,
-          exampleEntity.ATypeField);
+                                 exampleEntity.ATypeField);
       case B -> Projections.bean(ExampleEntityDto.class, exampleEntity.commonField,
-          exampleEntity.BTypeField);
+                                 exampleEntity.BTypeField);
       case C -> Projections.bean(ExampleEntityDto.class, exampleEntity.commonField,
-          exampleEntity.CTypeField);
+                                 exampleEntity.CTypeField);
     };
   }
 }
